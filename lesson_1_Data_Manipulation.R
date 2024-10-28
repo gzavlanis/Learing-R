1a653717c32120ab89780b61612e1528a16df9b3
George Zavlanis <90002254+gzavlanis@users.noreply.github.com>
2024-10-28 20:30:04 +0200

    finish studying part 1

--- lesson_1_Data_Manipulation.R
+++ lesson_1_Data_Manipulation.R
@@ -77,3 +77,65 @@ names(cereals)[2] <- "Producer"
 print(names(cereals))
 
 # Data visualization
+
+# Bar Chart
+y <- c(35, 80, 20, 62, 90, 40)
+x <- c("a", "b", "c", "d", "e", "f")
+barplot(y, names.arg = x, xlab = "categorical attribute", ylab = "frequency")
+
+# Histogram
+hist(iris$Petal.Length)
+hist(iris$Petal.Length, breaks = c(1.0, 3.0, 4.0, 4.5, 6.9))
+
+# Boxplot
+boxplot(iris$Sepal.Length ~ iris$Species)
+
+# Boxplot using a random sample from a standard normal distribution
+norm100 <- rnorm(100, 0, 1)
+norm1000 <- rnorm(1000, 0, 1)
+boxplot(norm100, norm1000, notch = TRUE, names = c("n=100", "n=1000"))
+
+# Boxplot using a random sample from an exponential distribution
+exp1000 <- rexp(n = 1000, rate = 1)
+exp100 <- rexp(n = 100, rate = 1)
+boxplot(exp1000, exp100, notch = TRUE, names = c("n=1000", "n=100"))
+
+# Exercises:
+data(islands)
+print(length(islands))
+
+print(mean(islands))
+print(median(islands))
+
+print(range(islands)[1])
+print(range(islands)[2])
+
+print(sd(islands))
+print(quantile(islands))
+print(quantile(islands, c(.05, .95)))
+print(IQR(islands))
+
+hist(islands)
+hist(islands, prob = TRUE)
+
+boxplot(islands)
+boxplot(islands, outline = FALSE)
+
+# Visualization Methods for Two Attributes
+
+# Scatter Plot
+plot(
+    jitter(iris$Sepal.Length), jitter(iris$Sepal.Width),
+    pch = c(23, 24, 25)[unclass(iris$Species)],
+    bg = c("red", "green3", "blue")[unclass(iris$Species)]
+)
+
+plot(
+    jitter(iris$Petal.Length), jitter(iris$Petal.Width),
+    pch = c(23, 24, 25)[unclass(iris$Species)],
+    bg = c("red", "green3", "blue")[unclass(iris$Species)]
+)
+legend(
+    5, 0.7, legend = c("Virginica", "Versicolor", "Setosa"),
+    fill = c("blue", "green3", "red")
+)
