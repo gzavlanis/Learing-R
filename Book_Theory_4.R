299323f70e4f0102e149ee86b5b9548412874d37
George Zavlanis <90002254+gzavlanis@users.noreply.github.com>
2025-01-19 01:25:12 +0200

    Updates

--- Book_Theory_4.R
+++ Book_Theory_4.R
@@ -8,3 +8,22 @@ prop.train <- 2/3
 k <- round(prop.train*n)
 iris.training <- iris.shuffled[1:k,]
 iris.test <- iris.shuffled[(k + 1):n,]
+
+# Data preparation using R:
+# Missing values:
+x <- c(3, 2, NA, 4, NA, 1, NA, NA, 5)
+mean(x)
+mean(x, na.rm = T)
+
+# Normalization and Scaling:
+
+iris.norm <- iris
+
+for (i in c(1:length(iris.norm))) {
+  if (!is.factor(iris.norm[,i])) {
+    attr.mean <- mean(iris.norm[,i])
+    attr.sd <- sd(iris.norm[,i])
+    iris.norm[,i] <- (iris.norm[,i] - attr.mean) / attr.sd
+  }
+}
+
