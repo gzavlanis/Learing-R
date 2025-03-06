405262fbd9041dc6d14bbcd9654e6affe0de6198
George Zavlanis <90002254+gzavlanis@users.noreply.github.com>
2025-03-06 23:48:33 +0200

    finish the code part

--- Book_Theory_5.R
+++ Book_Theory_5.R
@@ -4,6 +4,7 @@ library("rpart")
 library("rpart.plot")
 library(e1071)
 library(lattice)
+library(ROCR)
 
 play_decision <- read.table("./Data/DTdata.csv", header = TRUE, sep = ",")
 play_decision
@@ -104,7 +105,7 @@ qqline(results2$residuals)
 # Logistic Regression example
 churn_input <- data.frame(
   ID = c(1, 2, 3, 4, 5, 6),
-  Churned = c(0, 0, 0, 0, 0, 0),
+  Churned = c(0, 0, 0, 0, 0, 1),
   Age = c(61, 50, 47, 50, 29, 43),
   Married = c(1, 1, 1, 1, 1, 1),
   Cust_years = c(3, 3, 2, 3, 1, 4),
@@ -113,8 +114,35 @@ churn_input <- data.frame(
 head(churn_input)
 sum(churn_input$Churned)
 
-Churn_logistic1 <- glm (Churned ~ Age + Married + Cust_years + Churned_contacts, data = churn_input, family = binomial(link = "logit"))
+Churn_logistic1 <- glm(Churned ~ Age + Married + Cust_years + Churned_contacts, data = churn_input, family = binomial(link = "logit"))
 summary(Churn_logistic1)
 
 Churn_logistic3 <- glm(Churned ~ Age + Churned_contacts, data = churn_input, family = binomial(link = "logit"))
 summary(Churn_logistic3)
+
+pred = predict(Churn_logistic3, type = "response")
+predObj = prediction(pred, churn_input$Churned)
+rocObj = performance(predObj, measure = "tpr", x.measure = "fpr")
+aucObj = performance(predObj, measure = "auc")
+plot(rocObj, main = paste("Area under the curve:", round(aucObj@y.values[[1]], 4)))
+
+# extract the alpha(threshold), FPR, and TPR values from rocObj
+alpha <- round(as.numeric(unlist(rocObj@alpha.values)), 4)
+fpr <- round(as.numeric(unlist(rocObj@x.values)), 4)
+tpr <- round(as.numeric(unlist(rocObj@y.values)), 4)
+
+# adjust margins and plot TPR and FPR
+par(mar = c(5,5,2,5))
+plot(alpha, tpr, xlab = "Threshold", xlim = c(0, 1), ylab = "True positive rate", type = "l")
+par(new = "True")
+plot(alpha, fpr, xlab = "", ylab = "", axes = F, xlim = c(0, 1), type = "l" )
+axis(side = 4)
+mtext(side = 4, line = 3, "False positive rate")
+text(0.18, 0.18, "FPR")
+text(0.58, 0.58, "TPR")
+
+i <- which(round(alpha, 2) == .5)
+paste("Threashold=", (alpha[i]), "TPR=", tpr[i], "FPR=", fpr[i])
+
+i <- which(round(alpha, 2) == .15)
+paste("Threashold=", (alpha[i]), "TPR=", tpr[i], "FPR=", fpr[i])
